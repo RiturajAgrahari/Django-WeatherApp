76e11795957bb54d4a321831650a0ef132f9f677
RiturajAgrahari <agr.rituraj@gmail.com>
2024-01-25 23:09:04 +0530

    uplaoding the finalized weatherapp

---
+++ weather_app/static/script.js
@@ -0,0 +1,61 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Get references to the HTML elements
+    var elementToChange = document.getElementById("main_body");
+    var changeClassLink = document.getElementById("toggle_theme");
+    var svg = document.getElementById("toggle_icon");
+
+    // Check if a theme is stored in local storage
+    var currentTheme = localStorage.getItem("theme") || "dark_theme";
+    elementToChange.classList.toggle(currentTheme);
+
+    // Add a click event listener to the anchor tag
+    changeClassLink.addEventListener("click", function(event) {
+        // Prevent the default behavior of the anchor tag
+        event.preventDefault();
+
+        if (elementToChange.classList.contains("dark_theme")) {
+            elementToChange.classList.toggle("light_theme");
+            elementToChange.classList.remove("dark_theme");
+            svg.innerHTML=`
+                <path d="M8 11a3 3 0 1 1 0-6 3 3 0 0 1 0 6m0 1a4 4 0 1 0 0-8 4 4 0 0 0 0 8M8 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 0m0 13a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 13m8-5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2a.5.5 0 0 1 .5.5M3 8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2A.5.5 0 0 1 3 8m10.657-5.657a.5.5 0 0 1 0 .707l-1.414 1.415a.5.5 0 1 1-.707-.708l1.414-1.414a.5.5 0 0 1 .707 0m-9.193 9.193a.5.5 0 0 1 0 .707L3.05 13.657a.5.5 0 0 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0m9.193 2.121a.5.5 0 0 1-.707 0l-1.414-1.414a.5.5 0 0 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .707M4.464 4.465a.5.5 0 0 1-.707 0L2.343 3.05a.5.5 0 1 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .708"/>
+                `
+            svg.classList.toggle("bi-brightness-high");
+            svg.classList.remove("bi-moon-stars");
+            localStorage.setItem("theme", "light_theme");
+        }
+        else {
+            elementToChange.classList.toggle("dark_theme");
+            elementToChange.classList.remove("light_theme");
+            svg.innerHTML=`
+                  <path d="M6 .278a.77.77 0 0 1 .08.858 7.2 7.2 0 0 0-.878 3.46c0 4.021 3.278 7.277 7.318 7.277q.792-.001 1.533-.16a.79.79 0 0 1 .81.316.73.73 0 0 1-.031.893A8.35 8.35 0 0 1 8.344 16C3.734 16 0 12.286 0 7.71 0 4.266 2.114 1.312 5.124.06A.75.75 0 0 1 6 .278M4.858 1.311A7.27 7.27 0 0 0 1.025 7.71c0 4.02 3.279 7.276 7.319 7.276a7.32 7.32 0 0 0 5.205-2.162q-.506.063-1.029.063c-4.61 0-8.343-3.714-8.343-8.29 0-1.167.242-2.278.681-3.286"/>
+                  <path d="M10.794 3.148a.217.217 0 0 1 .412 0l.387 1.162c.173.518.579.924 1.097 1.097l1.162.387a.217.217 0 0 1 0 .412l-1.162.387a1.73 1.73 0 0 0-1.097 1.097l-.387 1.162a.217.217 0 0 1-.412 0l-.387-1.162A1.73 1.73 0 0 0 9.31 6.593l-1.162-.387a.217.217 0 0 1 0-.412l1.162-.387a1.73 1.73 0 0 0 1.097-1.097zM13.863.099a.145.145 0 0 1 .274 0l.258.774c.115.346.386.617.732.732l.774.258a.145.145 0 0 1 0 .274l-.774.258a1.16 1.16 0 0 0-.732.732l-.258.774a.145.145 0 0 1-.274 0l-.258-.774a1.16 1.16 0 0 0-.732-.732l-.774-.258a.145.145 0 0 1 0-.274l.774-.258c.346-.115.617-.386.732-.732z"/>
+                `
+            svg.classList.toggle("bi-moon-stars");
+            svg.classList.remove("bi-brightness-high");
+            localStorage.setItem("theme", "dark_theme");
+        }
+        // Toggle the class of the target element
+
+    });
+});
+
+// Function to close the error message after a specified time (e.g., 5000 milliseconds or 5 seconds)
+function closeErrorMessage() {
+    var errorMessage = document.getElementById('error-message');
+    errorMessage.classList.toggle('error-hidden');
+    errorMessage.classList.remove("error");
+}
+
+
+
+
+
+
+
+
+
+//(positive) --> 0  1  2  3 4 5 6 7 8 9 10 11 12 [-5:-2] = [len(word)-5:len(word)-2] = [13 - 5: 13 - 2] = [8 : 11]
+//(word)     --> H  e  l  l o ,   W o r l  d  !
+//(negative) --> 13 12 11 10 9 8 7 6 5 4 3 2  1 [-5 : -2]
+
+

523a67c6c9e7379f7f806062cc0a6659668c4e6f
RiturajAgrahari <agr.rituraj@gmail.com>
2025-10-14 22:49:29 +0530

    removed database as cauing error with vercel

--- weather_project/settings.py
+++ weather_project/settings.py
@@ -65,12 +65,7 @@ WSGI_APPLICATION = 'weather_project.wsgi.app'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+DATABASES = {}
 
 
 # Password validation

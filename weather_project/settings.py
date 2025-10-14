d0a0dc95f669e4893711195c5ce8124e065b9386
RiturajAgrahari <agr.rituraj@gmail.com>
2025-10-14 23:01:32 +0530

    Allowed host should have the vercel domain

--- weather_project/settings.py
+++ weather_project/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = 'django-insecure-bjolc)_00d18fd0=g@8vn=-^xe0-o$db)q(yu1d^t*39k%tr9s
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 
 # Application definition
 
